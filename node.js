4bca96b4ad00949ba096d1434958b6ea26c6b99b
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-04-28 14:35:58 -0400

    adding challenges

--- node.js
+++ node.js
@@ -147,3 +147,20 @@ function minMax(arr) {
                   
                   console.log(countVowels("Celebration"))
                     // ➞ 5
+
+                    function sortByLength(arr) {
+                        for(let i =0;i<arr.length;i++){
+                          for( let j=0;j<arr.length-i -1;j++){
+                            var temp= arr[j]
+                            if(arr[j].length> arr[j+1].length){
+                              arr[j]=arr[j+1];
+                              console.log(arr[j+1])
+                              arr[j+1]= temp
+                            }
+                          }
+                        }
+                         return arr
+                          }
+                       
+                       console.log(sortByLength(["Google", "Apple", "Microsoft"]))
+                       // ➞ ["Apple", "Google", "Microsoft"]
