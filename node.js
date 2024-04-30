d0b1f2b0ea46aada415424bf4eba77fffed733c0
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-04-30 18:38:01 -0400

    adding fibonacci

--- node.js
+++ node.js
@@ -234,3 +234,15 @@ function minMax(arr) {
                               
                               console.log(removeVowels("I have never seen a thin person drinking Diet Coke."))
                               // ➞ " hv nvr sn  thn prsn drnkng Dt Ck."
+
+                              function fibonacci(n){
+                                const fib=[0,1];
+                                for(let i =2;i<n;i++){
+                                  fib[i]=fib[i-1]+ fib[i-2];
+                                }
+                                return fib
+                              }
+                              
+                              console.log(fibonacci(3)) 
+                              // [0,1,1]
+                            //   learning fibonacci sequence 
