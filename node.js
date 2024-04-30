e5e294724090e7caf86c4f8c6b3d390d081d2fe6
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-04-30 16:50:45 -0400

    adding values

--- node.js
+++ node.js
@@ -180,3 +180,23 @@ function minMax(arr) {
                     
                     console.log(getAbsSum([2, -1, 4, 8, 10]))
                       // ➞ 25
+
+                      function sortNumsAscending(arr) {
+                        let n = arr.length;
+                        for(let i =0;i<n-1;i++){
+                          for(let j =0;j<n-i-1;j++){
+                            if(arr[j]>arr[j+1]){
+                              let temp =arr[j];
+                              arr[j]= arr[j+1];
+                              arr[j+1]= temp
+                            }
+                            
+                          }
+                        }
+                        return arr
+                      }
+                      
+                      console.log(sortNumsAscending([1, 2, 10, 50, 5]))
+                        // ➞ [1, 2, 5, 10, 50]
+
+                        // I just did bubble sort
