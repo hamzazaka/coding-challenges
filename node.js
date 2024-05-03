211bc91af1a184692e35546c5f5fcf72f418bfe2
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-05-03 15:51:13 -0400

    implemented calculator here

--- node.js
+++ node.js
@@ -364,3 +364,18 @@ function factorial(f){
           
           // console.log(num_of_digits(-2147483647))
             // ➞ 
+
+            function calculator(num1, operator, num2) {
+                switch (operator) {
+                    case '+':
+                        return num1 + num2;
+                    case '-':
+                        return num1 - num2;
+                    case '*':
+                        return num1 * num2;
+                    case '/':
+                        if (num2 === 0) return "Can't divide by 0!";
+                        return num1 / num2;
+                }
+                return null;
+            }
