10276b092fe880ba627f160cb90af5246402f8de
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-05-16 16:09:25 -0400

    adding values

--- node.js
+++ node.js
@@ -523,5 +523,18 @@ function factorial(f){
                     
                     console.log(sumTwoSmallestNums([2, 9, 6, -1]) )
                       // ➞ 8
+
+                      function factorize(num) {
+                        let factors =[];
+                        for(let i =1;i<=num;i++){
+                          if(num%i===0){
+                            factors.push(i)
+                          }
+                        }
+                        return factors
+                      }
+                      
+                      console.log(factorize(12))
+                        // ➞ [1, 2, 3, 4, 6, 12]
                     
                    
