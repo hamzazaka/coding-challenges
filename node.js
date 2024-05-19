f1004484e1514df1345d20a1c739b30e65f28d9b
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-05-19 16:23:35 -0400

    adding values

--- node.js
+++ node.js
@@ -598,3 +598,20 @@ function factorial(f){
                           
                           console.log(sevenBoom([2, 55, 60, 97, 86]) )
                             // ➞ "Boom!"
+
+
+function insertionSort(arr) {
+  for(let i =1;i<arr.length;i++){
+    let numberToInsert=arr[i]
+    let j=i-1
+    while (j>=0 && arr[j]> numberToInsert){
+      arr[j+1]=arr[j]
+      j=j-1
+    }
+    arr[j+1]=numberToInsert
+  }
+};
+
+const arr = [8,20,-2,4,-6]
+insertionSort(arr);
+console.log(arr)
