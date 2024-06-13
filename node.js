c8849a322847093d2ae6bf82390cc840b1c6cc69
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-06-12 23:25:21 -0400

    empty spaces

--- node.js
+++ node.js
@@ -719,6 +719,8 @@ console.log(insertWhitespace("SheWalksToTheBeach"))
 
 
    console.log(reverseWords('i.like.this.program.very.much'))
+   console.log(reverseWords('i.like.this.program.very.much'))
+
 
 
     
