c682a11fdc8c96b14b4d0f52b59180f109e07e63
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-06-18 14:50:46 -0400

    woking on console

--- node.js
+++ node.js
@@ -731,7 +731,8 @@ console.log(insertWhitespace("SheWalksToTheBeach"))
 
    console.log(reverseWords('i.like.this.program.very.much'))
    console.log(reverseWords('i.like.this.program.very.much'))
-
+   console.log(reverseWords('i.like.this.program.very.much'))
+   console.log(reverseWords('i.like.this.program.very.much'))
 
 
     
