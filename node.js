7847dd775baebd061f29c34ab6cd8aeca8d7b06f
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-08-18 13:18:04 -0400

    adding challenges

--- node.js
+++ node.js
@@ -752,6 +752,7 @@ function sevenBoom(arr) {
 console.log(sevenBoom([2, 55, 60, 97, 86]))
 console.log(sevenBoom([2, 55, 60, 97, 86]))
 console.log(sevenBoom([2, 55, 60, 97, 86]))
+console.log(sevenBoom([2, 55, 60, 97, 86]))
 
 
   // ➞ "Boom!"
