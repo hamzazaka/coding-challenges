9dec341de7ef0ec9cc4c0ce95e4c1a7819bea6a8
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-09-20 16:02:03 -0400

    adding values

--- node.js
+++ node.js
@@ -784,6 +784,9 @@ console.log(sevenBoom([2, 55, 60, 97, 86]))
 console.log(sevenBoom([2, 55, 60, 97, 86]))
 console.log(sevenBoom([2, 55, 60, 97, 86]))
 console.log(sevenBoom([2, 55, 60, 97, 86]))
+console.log(sevenBoom([2, 55, 60, 97, 86]))
+console.log(sevenBoom([2, 55, 60, 97, 86]))
+
 
 
 
