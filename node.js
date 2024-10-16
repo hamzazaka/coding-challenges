9414a5b392b840456cf6a583376dadcecc8b3b72
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-10-16 15:31:06 -0400

    addin values

--- node.js
+++ node.js
@@ -792,6 +792,8 @@ console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
+console.log(minMax([1, 2, 3, 4, 5]))
+console.log(minMax([1, 2, 3, 4, 5]))
 
 
 
