c311c8fb733c67727393c54d38accc55cfca938f
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-10-20 21:23:38 -0400

    added log

--- node.js
+++ node.js
@@ -800,6 +800,9 @@ console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
+console.log(minMax([1, 2, 3, 4, 5]))
+console.log(minMax([1, 2, 3, 4, 5]))
+
 
 
 
