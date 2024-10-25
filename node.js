7a8f6b9e980862e67ca28e8dd8a1f736f706ad84
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-10-25 13:04:00 -0400

    added values

--- node.js
+++ node.js
@@ -804,6 +804,9 @@ console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([1, 2, 3, 4, 5]))
+console.log(minMax([1, 2, 3, 4, 5]))
+console.log(minMax([1, 2, 3, 4, 5]))
+
 
 
 
