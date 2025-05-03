402a5edfa1e901e32287f3e6ca5594eb3368aaa9
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2025-05-03 12:43:48 -0400

    added extension

--- 2025/arrays.js
+++ 2025/arrays.js
@@ -92,7 +92,6 @@ const largestArr =(arr)=>{
 const secLargestArr=(arr)=>{
     let first = -1
     let second = -1
-
     for(let i=0;i<arr.length;i++){
         if(arr[i]>first){
             second=first
